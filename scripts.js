8e2cd31405fdd0971ddecdb3a78425fbbba898b0
Chris <chris.anderson_ext@augustwenty.com>
2020-12-28 14:18:08 -0600

    updated site

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const navigateToBlogPost = function(event) {
+    window.location.href = 'http://www.thelink.com' 
+    return false;
+  }
+  document.getElementById("flex").onclick = navigateToBlogPost;

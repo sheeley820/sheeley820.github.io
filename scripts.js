2e206739302239018fdb45da3a2c0fe37e5489cc
Gabriel Sheeley <iammcgaber@gmail.com>
2021-01-05 15:44:03 -0500

    Add ability to filter blog posts by tag

--- scripts.js
+++ scripts.js
@@ -1,5 +1,31 @@
-const navigateToBlogPost = function(event) {
-    window.location.href = 'http://www.thelink.com' 
-    return false;
+let tags = document.getElementsByClassName("tag-container")
+
+const filterFunc = function(arg) {
+  for(let tag of tags) {
+    let tagFound = false;
+    for(child of tag.children) {
+      if (arg.innerText === child.innerText) {
+        tagFound = true
+      }
+    }
+    if (!tagFound) {
+      tag.closest(".archive-entry").classList.add("collapse")
+    } else {
+      tag.closest(".archive-entry").classList.remove("collapse")
+    }
+    console.log(tag)
   }
-  document.getElementById("flex").onclick = navigateToBlogPost;
+  
+}
+
+//How do we retrive all the tags present
+//   - Retrieve the divs that contain the tags by giving them a tag-container class name, then go through child elements
+//Do do we identify the card that tag lives in?
+//   - container.closest(".archive-entry")
+//How do we hide those cards?
+//   - let col = document.getElementById("collapse-me").classList.remove("collapse")
+//   - let col = document.getElementById("collapse-me").classList.add("collapse")
+
+//if (a.innerText !== b.innerText) {
+  // hide the thing
+// }

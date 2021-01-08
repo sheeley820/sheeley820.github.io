91d4691836fd11713258e2a62a893a1600b6bfd8
Gabriel Sheeley <iammcgaber@gmail.com>
2021-01-08 14:58:42 -0500

    A start

---
+++ scripts/archives.js
@@ -0,0 +1,4 @@
+let archiveList = [
+    '<li><a class="dropdown-item" href="./archive/walkthrough.html">Red Dead Redemption Walkthrough</a></li>',
+    '<li><a class="dropdown-item" href="./archive/devStories.html">Red Dead Redemption Development Stories</a></li>'
+]

f518e8ad5d95bd9c25c820bdd5103b37c75f86ae
Chris <chris.anderson_ext@augustwenty.com>
2021-01-08 14:41:29 -0600

    added dynamic nav bar dropdown items

--- scripts/archives.js
+++ scripts/archives.js
@@ -2,3 +2,14 @@ let archiveList = [
     '<li><a class="dropdown-item" href="./archive/walkthrough.html">Red Dead Redemption Walkthrough</a></li>',
     '<li><a class="dropdown-item" href="./archive/devStories.html">Red Dead Redemption Development Stories</a></li>'
 ]
+
+//the diveder to which we will insert adjacent html 'after end'
+let divider = document.getElementById("dropDiv")
+
+//function executing on load to populate archive posts into dropdown menu
+window.onload = () => {
+for (let i = 0; i < archiveList.length; i++) {
+    divider.insertAdjacentHTML('afterend', archiveList[i])
+    }
+}
+// function will sort archives newest => oldest, meaning last item in archive list will be at top of dropdown menu

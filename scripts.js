a6ec45b77c5617455d075c63e6c1ebfeb9d4115c
Chris <chris.anderson_ext@augustwenty.com>
2021-01-08 11:14:56 -0600

    added all tag, cards now link to archive pages

--- scripts.js
+++ scripts.js
@@ -25,13 +25,23 @@ const filterFunc = function(arg) {
   }
 }
 
-//TODO
-//-  Make the tag button look clicked or active to show what tag is actively being filtered
-//  - Add data-bs-toggle="button" to our button.
-//  FIX-ME
-//  - Two tag buttons can appear selected, even though only one tag genre is being shown
-//  - When a button is deselected (clicked twice) that tag genre is still shown.  Clicking it again should make all block posts re-appear.
-//
-//-  Have a way to show all blog posts again
-//-  Build our list of Tag buttons based on the tags that exist in the blog posts (except maybe the all tag)
-//
+const showAll = (arg) => {
+  for(let tag of tags) {
+    tag.closest(".archive-entry").classList.remove("collapse")
+  }
+}
+
+
+
+//DONE - chris friday morning
+//Add an "All" tag button to the blog that shows all the blog posts. DONE 
+//Add an HTML page for each archived blog post.  At present you can't really click on them.
+//Make some way for the user to navigate to each archived blog post.  You can choose how this is done.  Will the whole card be clickable, or just the photo, blog post title, etc.
+//The pages for each blog post should look consistent with our other pages, meaning it should have the navbar and footer present, and looking the same.
+
+//New TODO
+//Flesh out the archive pages with more styling, images, etc.
+//"Archive" on header could have a drop down that shows all archive pages?
+//Build our list of Tag buttons based on the tags that exist in the blog posts (except maybe the all tag), also maybe make tags make more sense with what archive stories are
+//Tags on the right side of the cards could also be clickable, so that selecting tag at top OR on the card filters tags
+
